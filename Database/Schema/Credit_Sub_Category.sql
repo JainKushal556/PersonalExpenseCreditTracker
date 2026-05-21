2cc5bba38ae8bacdda35e2c00117ada66d750f67
arpitajana207 <titlijana1234@gmail.com>
2026-05-22 00:43:08 +0530

    Added Credit_Sub_Category Table

---
+++ Database/Schema/Credit_Sub_Category.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Credit_Sub_Category(
+  Sub_Category_ID	INT	PRIMARY KEY IDENTITY(1,1),
+  Category_ID	INT NOT NULL,
+  Sub_Category_Name	VARCHAR(100) UNIQUE NOT NULL,
+  FOREIGN KEY (Category_ID)
+  REFERENCES Credit_Category(Category_ID)
+)
