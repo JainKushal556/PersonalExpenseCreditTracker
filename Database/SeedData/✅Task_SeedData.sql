4e0baff41aad358f6cba92f856d4d2d52b78782b
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-22 13:51:59 +0530

    Task table data inserted

---
+++ Database/SeedData/✅Task_SeedData.sql
@@ -0,0 +1,13 @@
+INSERT INTO Task (User_ID, Priority_ID, Status_ID, Task_Title, Deadline)
+VALUES
+(1, 1, 1, 'Complete  Assignment', '2026-05-25'),
+(2, 2, 1, 'Prepare Project Work', '2026-05-26'),
+(3, 3, 2, 'Database table create', '2026-05-27'),
+(4, 1, 2, 'Database table data Insert', '2026-05-28'),
+(5, 2, 1, 'Buy Grocery Items', '2026-05-29'),
+(6, 3, 2, 'Pay Electricity Bill', '2026-05-30'),
+(7, 1, 1, 'Submit College Assignment', '2026-05-31'),
+(8, 2, 2, 'Clean Study Room', '2026-06-01'),
+(9, 3, 1, 'Wash Clothes', '2026-06-02'),
+(10, 1, 2, 'Optimize SQL Queries', '2026-06-03');
+
