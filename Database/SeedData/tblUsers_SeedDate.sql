d1d8cd42af271e85bb94a2d850d98dc96f32210e
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-23 01:05:00 +0530

    tblUsers table 10 data inserted

---
+++ Database/SeedData/tblUsers_SeedDate.sql
@@ -0,0 +1,13 @@
+INSERT INTO tblUsers (UserName, CreatedAt)
+VALUES
+('Sujit Kar', GETDATE()),
+('Kushal Jain', GETDATE()),
+('Debojyoti Jana', GETDATE()),
+('Arindam Sahoo', GETDATE()),
+('Deepjyoti Das', GETDATE()),
+('Arpita Jana', GETDATE()),
+('Sampriti Maity', GETDATE()),
+('Rahul Das', GETDATE()),
+('Priya Sharma', GETDATE()),
+('Anik Paul', GETDATE());
+
