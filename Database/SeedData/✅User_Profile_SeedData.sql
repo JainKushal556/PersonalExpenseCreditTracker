baa65de23f2941016a2cbdc5c6875ea6a255aeac
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-22 13:19:38 +0530

    User_Profile table 10 data inserted

---
+++ Database/SeedData/✅User_Profile_SeedData.sql
@@ -0,0 +1,13 @@
+INSERT INTO User_Profile (User_ID, Name, Profile_Photo)
+VALUES
+(1, 'Sujit Kar', 0xFFD8FFE000104A464946),
+(2, 'Kushal Jain', 0x89504E470D0A1A0A),
+(3, 'Debojyoti Jana', 0x474946383961),
+(4, 'Arindam Sahoo', 0xFFD8FFE1001845786966),
+(5, 'Deepjyoti Das', 0x89504E470D0A1A0A),
+(6, 'Arpita Jana', 0x474946383761),
+(7, 'Rohit Singh', 0xFFD8FFE000104A464946),
+(8, 'Puja Das', 0x89504E470D0A1A0A),
+(9, 'Arijit Bose', 0x474946383961),
+(10, 'Neha Gupta', 0xFFD8FFE1001845786966);
+
