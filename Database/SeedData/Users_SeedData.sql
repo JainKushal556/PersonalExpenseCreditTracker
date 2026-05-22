3dd1c53cd4069064c1b0fb008f471769e88ffad5
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-22 13:00:55 +0530

    Users 10 data Insert

---
+++ Database/SeedData/Users_SeedData.sql
@@ -0,0 +1,15 @@
+
+select *from Users;
+
+INSERT INTO Users (User_Name, Created_At)
+VALUES
+('Sujit Kar', GETDATE()),
+('Kushal Jain', GETDATE()),
+('Debojyoti Jana', GETDATE()),
+('Arindam Sahoo', GETDATE()),
+('Deepjyoti Das', GETDATE()),
+('Arpita Jana', GETDATE()),
+('Rohit Singh', GETDATE()),
+('Puja Das', GETDATE()),
+('Arijit Bose', GETDATE()),
+('Neha Gupta', GETDATE());
