cf525f9b38609b6ce30d1a5a03c4b918b19434e5
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-22 13:26:43 +0530

    User_Contact table 10 data inserted

---
+++ Database/SeedData/✅User_Contact_SeedData.sql
@@ -0,0 +1,13 @@
+INSERT INTO User_Contact (User_ID, Email, Phone_Number)
+VALUES
+(1, 'sujitkar@gmail.com', '7047104672'),
+(2, 'kushaljain@gmail.com', '7679673882'),
+(3, 'debojyoti@gmail.com', '9382332862'),
+(4, 'arindamsahoo@gmail.com', '7407528941'),
+(5, 'deepjyoti@gmail.com', '9679786764'),
+(6, 'arpitajana@gmail.com', '8637574895'),
+(7, 'rohitsingh@gmail.com', '9876237615'),
+(8, 'pujadas@gmail.com', '9478533217'),
+(9, 'arijitbose@gmail.com', '9179523228'),
+(10, 'nehagupta@gmail.com', '9374743219');
+
