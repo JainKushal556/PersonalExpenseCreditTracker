34d00307537393b0fa4e0de2c27f1ae833e042be
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-22 13:39:46 +0530

    User_Authentication table 10 data inserted

---
+++ Database/SeedData/✅User_Authentication_SeedData.sql
@@ -0,0 +1,13 @@
+INSERT INTO User_Authentication (User_ID, Password, Active)
+VALUES
+(1, 'Sujit@123', 0),
+(2, 'Kushal@456', 0),
+(3, 'Debo@789', 0),
+(4, 'Arindam@321', 0),
+(5, 'Deep@654', 0),
+(6, 'Arpita@987', 0),
+(7, 'Rohit@741', 0),
+(8, 'Puja@852', 0),
+(9, 'Arijit@963', 0),
+(10, 'Neha@159', 0);
+
