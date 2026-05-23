91197101ae51b9a273bf8b1f2e87dbd1be026256
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-23 15:46:27 +0530

    Update Note table

--- Database/✔️Schema/✔️Note.sql
+++ Database/✔️Schema/✔️Note.sql
@@ -2,17 +2,17 @@ CREATE TABLE tblNote
 (
     NoteID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 
-    UserID INT NOT NULL ,
+    UserID INT NOT NULL,
     NoteStatusID INT NOT NULL,
 
-    NoteTitle VARCHAR(150) NOT NULL,
+    NoteTitle VARCHAR(MAX) NOT NULL,
     Description VARCHAR(MAX) NOT NULL,
-    CreatedAt DATETIME NOT NULL,
 
-    
-        FOREIGN KEY (UserID)
-        REFERENCES tblUsers(UserID),
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE(),
 
-        FOREIGN KEY (NoteStatusID)
-        REFERENCES tblNoteStatus(NoteStatusID)
+    FOREIGN KEY (UserID)
+    REFERENCES tblUsers(UserID),
+
+    FOREIGN KEY (NoteStatusID)
+    REFERENCES tblNoteStatus(NoteStatusID)
 );
