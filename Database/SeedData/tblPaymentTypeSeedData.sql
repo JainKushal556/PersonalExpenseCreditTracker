8a7534a17e09fdbd8b9679e78950c598399e22ee
arpitajana207 <titlijana1234@gmail.com>
2026-05-23 12:53:19 +0530

    tblPaymentType data inserted

---
+++ Database/SeedData/tblPaymentTypeSeedData.sql
@@ -0,0 +1,12 @@
+INSERT INTO tblPaymentType (PaymentName)
+VALUES
+('Cash'),
+('UPI'),
+('Credit Card'),
+('Debit Card'),
+('Net Banking'),
+('Google Pay'),
+('PhonePe'),
+('Paytm'),
+('Bank Transfer'),
+('Cheque');
