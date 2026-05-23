6c21e3480fa75ef0cfd3a56b795031d4cf57c19d
arpitajana207 <titlijana1234@gmail.com>
2026-05-23 12:44:56 +0530

    tblCredit table data inserted

---
+++ Database/SeedData/tblCreditSeedData.sql
@@ -0,0 +1,13 @@
+INSERT INTO tblCredit
+(UserID, SubCategoryID, PaymentID, Amount, Description)
+VALUES
+(1, 1, 1, 25000.00, 'Monthly salary credited'),
+(2, 2, 2, 18000.00, 'Income from clothing business'),
+(3, 3, 3, 5000.00, 'Stock market profit'),
+(4, 4, 1, 2000.00, 'Birthday gift received'),
+(5, 5, 2, 7500.00, 'Freelance web development project'),
+(6, 6, 1, 12000.00, 'House rent received'),
+(7, 7, 2, 600.00, 'Cashback from online shopping'),
+(8, 8, 3, 10000.00, 'Scholarship credited'),
+(9, 9, 1, 3500.00, 'Festival performance bonus'),
+(10, 10, 2, 1500.00, 'Refund from cancelled order');
