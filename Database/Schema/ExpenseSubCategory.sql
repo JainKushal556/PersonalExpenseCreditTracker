caecdedefa11f58dc39a90bb7e9c5572f04ee266
aniket406134 <pramanikaniket96@gmail.com>
2026-05-23 12:21:56 +0530

    expense sub category

--- Database/Schema/ExpenseSubCategory.sql
+++ Database/Schema/ExpenseSubCategory.sql
@@ -1,8 +1,8 @@
-CREATE TABLE Expense_Sub_Category (
-    Sub_Category_Id INT PRIMARY KEY IDENTITY(1,1),
-    Category_Id INT NOT NULL,
-    Sub_Category_Name VARCHAR(100) UNIQUE NOT NULL,
+CREATE TABLE tblExpenseSubCategory (
+    SubCategoryID INT PRIMARY KEY IDENTITY(1,1),
+    CategoryID INT NOT NULL,
+    SubCategoryName VARCHAR(100) UNIQUE NOT NULL,
 
-    FOREIGN KEY (Category_Id)
-    REFERENCES Expense_Category(Category_Id)
+    FOREIGN KEY (CategoryID)
+    REFERENCES tblExpenseCategory(CategoryID)
 );
