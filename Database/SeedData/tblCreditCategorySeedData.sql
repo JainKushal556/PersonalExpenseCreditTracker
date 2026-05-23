c7db01592a51b0e6e845286b508a47081981ab6e
arpitajana207 <titlijana1234@gmail.com>
2026-05-23 12:33:31 +0530

    tblCreditCategory  table data inserted

---
+++ Database/SeedData/tblCreditCategorySeedData.sql
@@ -0,0 +1,12 @@
+ INSERT INTO tblCreditCategory (CategoryName)
+VALUES
+('Salary'),
+('Business'),
+('Investment'),
+('Gift'),
+('Freelancing'),
+('Rental'),
+('Cashback'),
+('Scholarship'),
+('Bonus'),
+('Refund');
