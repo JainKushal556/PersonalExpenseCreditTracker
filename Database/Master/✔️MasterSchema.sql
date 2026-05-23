4450152d605d37921fe2487ebf650d568091b549
Kushal Jain <jainkushal556@gmail.com>
2026-05-23 14:34:04 +0530

    Master Schema Finalized & DDL Queries Verified

---
+++ Database/Master/✔️MasterSchema.sql
@@ -0,0 +1,243 @@
+--Independent Tables
+
+CREATE TABLE tblUsers (
+    UserID INT PRIMARY KEY IDENTITY(1,1),
+    UserName VARCHAR(MAX) NOT NULL,
+    CreatedAt DATETIME NOT NULL DEFAULT GETDATE()
+);
+GO
+
+CREATE TABLE tblExpenseCategory (
+    CategoryID INT PRIMARY KEY IDENTITY(1,1),
+    CategoryName VARCHAR(100) UNIQUE NOT NULL
+);
+GO
+
+Create Table tblPaymentType(
+    PaymentID INT PRIMARY KEY IDENTITY(1,1),
+    PaymentName VARCHAR(50) UNIQUE NOT NULL 
+);
+GO
+
+Create Table tblCreditCategory(
+    CategoryID  INT PRIMARY KEY IDENTITY(1,1),
+	CategoryName VARCHAR(100) UNIQUE Not Null
+);
+GO
+
+CREATE TABLE tblLentPersons(
+	PersonID INT PRIMARY KEY IDENTITY(1,1),
+	PersonName VARCHAR(100) NOT NULL,
+	PhoneNumber VARCHAR(15) NOT NULL,
+	Address VARCHAR(MAX)
+);
+GO
+
+CREATE TABLE tblLentBorrowStatus(
+	StatusID INT PRIMARY KEY IDENTITY(1,1),
+	StatusName VARCHAR(50) NOT NULL
+);
+GO
+
+CREATE TABLE tblTaskPriorities (
+    PriorityID INT PRIMARY KEY IDENTITY(1,1),
+    PriorityName VARCHAR(50) NOT NULL UNIQUE
+);
+GO
+
+CREATE TABLE tblTaskStatus (
+    TaskStatusID INT PRIMARY KEY IDENTITY(1,1),
+    TaskStatusName VARCHAR(50) NOT NULL UNIQUE
+);
+GO
+
+CREATE TABLE tblNoteStatus
+(
+NoteStatusID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+StatusName VARCHAR(50) UNIQUE NOT NULL
+);
+GO
+
+
+--Dependent Tables
+
+CREATE TABLE tblUserProfile (
+    ProfileID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT NOT NULL,
+    Name VARCHAR(MAX) NOT NULL,
+    ProfilePhoto VARBINARY(MAX) NULL,
+
+    FOREIGN KEY (UserID) REFERENCES tblUsers(UserID)
+);
+GO
+
+CREATE TABLE tblUserContact (
+    ContactID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT NOT NULL,
+    Email VARCHAR(100) NOT NULL UNIQUE,
+    PhoneNumber VARCHAR(15) NOT NULL UNIQUE,
+
+    FOREIGN KEY (UserID) REFERENCES tblUsers(UserID)
+);
+GO
+
+CREATE TABLE tblUserAuthentication (
+    AuthID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT NOT NULL,
+    Password VARCHAR(MAX) NOT NULL,
+    Active BIT NOT NULL,
+
+    FOREIGN KEY (UserID) REFERENCES tblUsers(UserID)
+);
+GO
+
+CREATE TABLE tblExpense (
+    ExpenseID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT NOT NULL,
+    CategoryID INT NOT NULL,
+    SubCategoryID INT NOT NULL,
+    Amount DECIMAL(10,2) NOT NULL,
+    Description VARCHAR(MAX) NOT NULL,
+    PaymentID INT NOT NULL,
+    ExpenseAt DATETIME DEFAULT GETDATE(),
+
+    FOREIGN KEY(UserID)
+    REFERENCES tblUsers(UserID),
+
+    FOREIGN KEY (CategoryID)
+    REFERENCES tblExpenseCategory(CategoryID),
+    
+
+    FOREIGN KEY (SubCategoryID)
+    REFERENCES tblExpenseSubCategory(SubCategoryID),
+    
+
+    FOREIGN KEY (PaymentID)
+    REFERENCES tblPaymentType(PaymentID)
+    
+);
+GO
+
+CREATE TABLE tblExpenseSubCategory (
+    SubCategoryID INT PRIMARY KEY IDENTITY(1,1),
+    CategoryID INT NOT NULL,
+    SubCategoryName VARCHAR(100) UNIQUE NOT NULL,
+
+    FOREIGN KEY (CategoryID)
+    REFERENCES tblExpenseCategory(CategoryID)
+);
+GO
+
+Create Table tblCredit(
+  CreditID INT PRIMARY KEY IDENTITY(1,1),
+  UserID  INT NOT NULL,
+  CategoryID INT NOT NULL,
+  SubCategoryID INT NOT NULL,
+  Amount DECIMAL(10,2) NOT NULL,
+  Description VARCHAR(MAX) NOT NULL,
+  PaymentID	INT NOT NULL,
+  CreditAt DATETIME DEFAULT GETDATE(),
+
+  FOREIGN KEY(CategoryID)
+  REFERENCES tblCreditCategory(CategoryID),
+
+  FOREIGN KEY(SubCategoryID)
+  REFERENCES tblCreditSubCategory(SubCategoryID),
+
+  FOREIGN KEY(UserID)
+  REFERENCES tblUsers(UserID),
+
+  FOREIGN KEY(PaymentID)
+  REFERENCES tblPaymentType(PaymentID)
+);
+GO
+
+Create Table tblCreditSubCategory(
+  SubCategoryID INT PRIMARY KEY IDENTITY(1,1),
+  CategoryID INT NOT NULL,
+  SubCategoryName VARCHAR(100) UNIQUE Not Null,
+
+  FOREIGN KEY(CategoryID)
+  REFERENCES tblCreditCategory(CategoryID)
+
+);
+GO
+
+CREATE TABLE tblLent(
+	LentID INT PRIMARY KEY IDENTITY(1,1),
+	UserID INT NOT NULL,
+	PersonID INT NOT NULL,
+	PaymentID INT NOT NULL,
+	StatusID INT NOT NULL,
+	Amount DECIMAL(10,2) NOT NULL,
+	LentAt DATETIME NOT NULL DEFAULT GETDATE(),
+	DeadlineAt DATETIME,
+	Description VARCHAR(MAX) NOT NULL,
+
+	FOREIGN KEY (UserID) REFERENCES tblUsers(UserID),
+	FOREIGN KEY (PersonID) REFERENCES tblLentPersons(PersonID),
+	FOREIGN KEY (PaymentID) REFERENCES tblPaymentType(PaymentID),
+	FOREIGN KEY (StatusID) REFERENCES tblLentBorrowStatus(StatusID)
+);
+GO
+
+CREATE TABLE tblBorrow (
+    BorrowID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT NOT NULL,
+    PersonID INT NOT NULL,
+    PaymentID INT NOT NULL,
+    StatusID INT NOT NULL,
+    Amount DECIMAL(10,2) NOT NULL,
+    BorrowAt DATETIME NOT NULL DEFAULT GETDATE(),
+    DeadlineAt DATETIME,
+    Description VARCHAR(MAX) NOT NULL,
+
+	FOREIGN KEY (UserID) REFERENCES tblUsers(UserID),
+	FOREIGN KEY (PersonID) REFERENCES tblBorrowPersons(PersonID),
+	FOREIGN KEY (PaymentID) REFERENCES tblPaymentType(PaymentID),
+	FOREIGN KEY (StatusID) REFERENCES tblLentBorrowStatus(StatusID)
+);
+GO
+
+CREATE TABLE tblBorrowPersons (
+    PersonID INT PRIMARY KEY IDENTITY(1,1),
+    PersonName VARCHAR(100) NOT NULL,
+    PhoneNumber VARCHAR(15) NOT NULL,
+    Address VARCHAR(MAX) NULL
+);
+GO
+
+CREATE TABLE tblTask (
+    TaskID INT PRIMARY KEY IDENTITY(1,1),
+    UserID INT NOT NULL,
+    PriorityID INT NOT NULL,
+    TaskStatusID INT NOT NULL,
+    TaskTitle VARCHAR(150) NOT NULL,
+    Deadline DATE NOT NULL,
+
+    FOREIGN KEY (UserID) REFERENCES tblUsers(UserID),
+    FOREIGN KEY (PriorityID) REFERENCES tblTaskPriorities(PriorityID),
+    FOREIGN KEY (TaskStatusID) REFERENCES tblTaskStatus(TaskStatusID)
+);
+GO
+
+CREATE TABLE tblNote
+(
+    NoteID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+
+    UserID INT NOT NULL ,
+    NoteStatusID INT NOT NULL,
+
+    NoteTitle VARCHAR(150) NOT NULL,
+    Description VARCHAR(MAX) NOT NULL,
+    CreatedAt DATETIME NOT NULL,
+
+    
+        FOREIGN KEY (UserID)
+        REFERENCES tblUsers(UserID),
+
+        FOREIGN KEY (NoteStatusID)
+        REFERENCES tblNoteStatus(NoteStatusID)
+);
+GO
+
