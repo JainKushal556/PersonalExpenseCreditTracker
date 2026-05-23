5f05e55ba5bab6b829365a88727884ebc3a45aec
arpitajana207 <titlijana1234@gmail.com>
2026-05-23 13:40:51 +0530

    tblExpenseSubCategory  table data inserted

---
+++ Database/SeedData/tblExpenseSubCategorySeedData.sql
@@ -0,0 +1,11 @@
+INSERT INTO tblExpenseSubCategory (CategoryID, SubCategoryName)
+VALUES
+(1, 'Restaurant'),
+(2, 'Bus Fare'),
+(3, 'Clothes'),
+(4, 'Electricity Bill'),
+(5, 'Movie'),
+(6, 'Medicine'),
+(7, 'Books'),
+(8, 'Fuel'),
+(9, 'Salon');
