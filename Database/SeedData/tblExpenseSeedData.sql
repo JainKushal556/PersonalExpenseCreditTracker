cc227d04ed38ca1d6e7e104e42277f544c7911af
arpitajana207 <titlijana1234@gmail.com>
2026-05-23 13:40:40 +0530

    tblExpense  table data inserted

---
+++ Database/SeedData/tblExpenseSeedData.sql
@@ -0,0 +1,12 @@
+INSERT INTO tblExpense
+(UserID,CategoryID, SubCategoryID, PaymentID, Amount, Description)
+VALUES
+(1,1, 1, 1, 350.00, 'Dinner at restaurant'),
+(2,2, 2, 2, 120.00, 'Daily bus travel'),
+(3,3, 3, 3, 2500.00, 'Purchased new clothes'),
+(4,4, 4, 4, 1800.00, 'Monthly electricity payment'),
+(5,5, 5, 5, 450.00, 'Movie ticket purchase'),
+(6,6, 6, 6, 900.00, 'Medicine from pharmacy'),
+(7,7, 7, 7, 1500.00, 'Bought academic books'),
+(8,8, 8, 8, 2200.00, 'Bike fuel expense'),
+(9,9, 9, 9, 700.00, 'Salon and grooming');
