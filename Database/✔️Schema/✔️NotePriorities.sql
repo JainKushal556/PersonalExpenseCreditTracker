8a6b9efc0e1768660c7e20138eba40ba35120e9f
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-24 21:01:17 +0530

    Update table

--- Database/✔️Schema/✔️NotePriorities.sql
+++ Database/✔️Schema/✔️NotePriorities.sql
@@ -1,4 +1,4 @@
-INSERT INTO tblNotePriorities (NotePriorityName)
-VALUES
-('Normal'),
-('Important');
+CREATE TABLE tblNotePriorities (
+    NotePriorityID INT PRIMARY KEY IDENTITY(1,1),
+    NotePriorityName VARCHAR(50) NOT NULL UNIQUE
+);
