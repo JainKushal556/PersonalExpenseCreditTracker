c41eba7b396ceed6170d2fb0596f0cfa0e0889d1
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-24 20:49:55 +0530

    Update table

--- Database/✔️Schema/✔️Note.sql
+++ Database/✔️Schema/✔️Note.sql
@@ -3,7 +3,7 @@ CREATE TABLE tblNote
     NoteID INT NOT NULL PRIMARY KEY IDENTITY(1,1),
 
     UserID INT NOT NULL,
-    NoteStatusID INT NOT NULL,
+    NotePriorityID INT NOT NULL,
 
     NoteTitle VARCHAR(MAX) NOT NULL,
     Description VARCHAR(MAX) NOT NULL,
@@ -13,6 +13,7 @@ CREATE TABLE tblNote
     FOREIGN KEY (UserID)
     REFERENCES tblUsers(UserID),
 
-    FOREIGN KEY (NoteStatusID)
-    REFERENCES tblNoteStatus(NoteStatusID)
+    FOREIGN KEY (NotePriorityID)
+    REFERENCES tblNotePriorities(NotePriorityID)
 );
+
