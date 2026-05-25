f3667630aad98f5eb0c4ea16061f957e148d5aa5
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-25 13:12:13 +0530

    spCheckActiveUserWithDetails Created

---
+++ Database/Procedures/Authentication/✅spCheckActiveUserWithDetails.sql
@@ -0,0 +1,47 @@
+CREATE PROCEDURE spCheckActiveUserWithDetails  
+(  
+    @UserID INT  
+)  
+AS  
+BEGIN  
+  
+    IF EXISTS  
+    (  
+        SELECT 1  
+        FROM tblUserAuthentication  
+        WHERE   
+            UserID = @UserID  
+            AND Active = 1  
+    )  
+    BEGIN  
+  
+        PRINT 'User Is Active';  
+  
+        SELECT   
+            U.UserID,  
+            U.UserName,  
+            P.Name,  
+            P.ProfilePhoto,  
+            C.Email,  
+            C.PhoneNumber,  
+            A.Active,  
+            U.CreatedAt  
+        FROM tblUsers U  
+        INNER JOIN tblUserProfile P  
+            ON U.UserID = P.UserID  
+        INNER JOIN tblUserContact C  
+            ON U.UserID = C.UserID  
+        INNER JOIN tblUserAuthentication A  
+            ON U.UserID = A.UserID  
+        WHERE   
+            U.UserID = @UserID  
+            AND A.Active = 1;  
+  
+    END  
+  
+    ELSE  
+    BEGIN  
+        PRINT 'User Is Not Active';  
+    END  
+  
+END;
