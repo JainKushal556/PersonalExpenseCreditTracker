e28d98dbb2e5a68c4a1f952130fb9840082f821a
Sk Akhmal Hossain <skakhmalhossain@gmail.com>
2026-05-25 17:59:36 +0530

    Update ✅spGetPendingLent.sql

--- Database/Procedures/Lent/✅spGetPendingLent.sql
+++ Database/Procedures/Lent/✅spGetPendingLent.sql
@@ -2,10 +2,10 @@ CREATE PROC spGetPendingLent
 @UserID INT
 AS
 BEGIN
-	SELECT p.PersonName, l.Amount, s.StatusName
-	FROM tblLent l
-	INNER JOIN tblUsers u ON l.UserID = u.UserID
-	INNER JOIN tblLentBorrowStatus s ON l.StatusID = s.StatusID
-	INNER JOIN tblLentPersons p ON l.PersonID = p.PersonID
-	WHERE u.UserID = @UserID AND s.StatusName = 'Pending';
+	SELECT Prns.PersonName, L.Amount, S.StatusName
+	FROM tblLent L
+	INNER JOIN tblUsers U ON L.UserID = U.UserID
+	INNER JOIN tblLentBorrowStatus S ON L.StatusID = S.StatusID
+	INNER JOIN tblLentPersons Prns ON L.PersonID = Prns.PersonID
+	WHERE U.UserID = @UserID AND S.StatusName = 'Pending';
 END
