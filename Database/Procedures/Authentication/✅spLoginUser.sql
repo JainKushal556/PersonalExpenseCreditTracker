f346c91fb039a83b4412d9d5c9522e108836b012
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-26 09:04:05 +0530

    STORE PROC UPDATE

--- Database/Procedures/Authentication/✅spLoginUser.sql
+++ Database/Procedures/Authentication/✅spLoginUser.sql
@@ -1,12 +1,13 @@
 CREATE PROCEDURE spLoginUser  
-(  
+
     @Email VARCHAR(100),  
     @Password VARCHAR(MAX)  
-)  
+
 AS  
 BEGIN  
-      
-  
+    
+    DECLARE @UserID INT;
+
     IF EXISTS  
     (  
         SELECT 1  
@@ -18,11 +19,28 @@ BEGIN
             AND A.Password = @Password  
     )  
     BEGIN  
-        PRINT 'User Exists';  
+
+        SELECT @UserID = C.UserID
+        FROM tblUserContact C
+        INNER JOIN tblUserAuthentication A  
+            ON C.UserID = A.UserID
+        WHERE   
+            C.Email = @Email  
+            AND A.Password = @Password;
+
+        UPDATE tblUserAuthentication
+        SET Active = 1
+        WHERE UserID = @UserID;
+
+        PRINT 'Login Successful';
+
+        SELECT @UserID AS UserID;
     END  
+    
     ELSE  
     BEGIN  
         PRINT 'Invalid Email Or Password';  
     END  
+
 END;
 
