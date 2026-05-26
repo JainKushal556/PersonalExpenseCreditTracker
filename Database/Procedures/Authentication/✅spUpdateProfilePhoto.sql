524db0300d305e68c372dc0e41d545d7a537dfbb
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-26 10:05:15 +0530

    STORE PROC UPDATE

--- Database/Procedures/Authentication/✅spUpdateProfilePhoto.sql
+++ Database/Procedures/Authentication/✅spUpdateProfilePhoto.sql
@@ -1,26 +1,45 @@
---UpdateUserProfilePhoto
-CREATE PROCEDURE spUpdateProfilePhoto  
-(  
-    @UserID INT,  
-    @ProfilePhoto VARBINARY(MAX)  
-)  
-AS  
-BEGIN  
-    IF EXISTS  
-    (  
-        SELECT 1  
-        FROM tblUserProfile  
-        WHERE UserID = @UserID  
-    )  
-    BEGIN  
-        UPDATE tblUserProfile  
-        SET ProfilePhoto = @ProfilePhoto  
-        WHERE UserID = @UserID;  
-  
-        PRINT 'Profile Photo Updated Successfully';  
-    END  
-    ELSE  
-    BEGIN  
-        PRINT 'User Not Found';  
-    END  
+CREATE PROCEDURE spUpdateProfilePhoto    
+    
+    @UserID INT,    
+    @ProfilePhoto VARBINARY(MAX)    
+    
+AS    
+BEGIN    
+
+   
+    IF NOT EXISTS
+    (
+        SELECT 1
+        FROM tblUserAuthentication
+        WHERE
+            UserID = @UserID
+            AND Active = 1
+    )
+    BEGIN
+        SELECT 'User Account Is Not Active' AS Message;
+        RETURN;
+    END
+
+
+    IF EXISTS    
+    (    
+        SELECT 1    
+        FROM tblUserProfile    
+        WHERE UserID = @UserID    
+    )    
+    BEGIN    
+
+        UPDATE tblUserProfile    
+        SET ProfilePhoto = @ProfilePhoto    
+        WHERE UserID = @UserID;    
+    
+        SELECT 'Profile Photo Updated Successfully' AS Message;
+
+    END    
+
+    ELSE    
+    BEGIN    
+        SELECT 'User Not Found' AS Message;
+    END    
+
 END;
