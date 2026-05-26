0593a4d2e64471eae8b5417249e0055e2f500a1b
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-26 22:00:14 +0530

    STORE PROC UPDATE

--- Database/Procedures/Authentication/❌spCheckActiveUserDetails.sql
+++ Database/Procedures/Authentication/❌spCheckActiveUserDetails.sql
@@ -1,50 +1,53 @@
-CREATE PROCEDURE spCheckActiveUserDetails  
-(  
-    @UserID INT  
-)  
-AS  
-BEGIN  
-  
-    IF EXISTS  
-    (  
-        SELECT 1  
-        FROM tblUserAuthentication  
-        WHERE   
-            UserID = @UserID  
-            AND Active = 1  
-    )  
-    BEGIN  
-  
-        SELECT   
-            'User Is Active' AS Message,
-            U.UserID,  
-            U.UserName,  
-            P.Name,  
-            P.ProfilePhoto,  
-            C.Email,  
-            C.PhoneNumber,  
-            A.Active,  
-            U.CreatedAt  
-        FROM tblUsers U  
-        INNER JOIN tblUserProfile P  
-            ON U.UserID = P.UserID  
-        INNER JOIN tblUserContact C  
-            ON U.UserID = C.UserID  
-        INNER JOIN tblUserAuthentication A  
-            ON U.UserID = A.UserID  
-        WHERE   
-            U.UserID = @UserID  
-            AND A.Active = 1;  
-  
-    END  
-  
-    ELSE  
-    BEGIN  
-        SELECT 'User Is Not Active' AS Message;
-    END  
-  
-END;
+ALTER PROCEDURE spCheckActiveUserDetails
+(
+    @UserID INT
+)
+AS
+BEGIN
 
---inner join er jaygay left join korar dekhbi inner join ee jodi user profile e ba contact er data soriye day to asbe na data bcz se all data pbe na 
---a.active pathate hbe na . 
---user name o dekhachis + name o dekhachis double hoye jche na 
+    -- User Exists Check
+    IF NOT EXISTS
+    (
+        SELECT 1
+        FROM tblUsers
+        WHERE UserID = @UserID
+    )
+    BEGIN
+        SELECT 'Invalid UserID' AS Message;
+        RETURN;
+    END
+
+
+    -- Active User Check
+    IF NOT EXISTS
+    (
+        SELECT 1
+        FROM tblUserAuthentication
+        WHERE UserID = @UserID
+        AND Active = 1
+    )
+    BEGIN
+        SELECT 'User Is Not Active' AS Message;
+        RETURN;
+    END
+
+
+    -- Active User Details
+    SELECT
+        U.UserID,
+        U.UserName,
+        P.ProfilePhoto,
+        C.Email,
+        C.PhoneNumber,
+        U.CreatedAt
+    FROM tblUsers U
+
+    LEFT JOIN tblUserProfile P
+        ON U.UserID = P.UserID
+
+    LEFT JOIN tblUserContact C
+        ON U.UserID = C.UserID
+
+    WHERE U.UserID = @UserID;
+
+END;
