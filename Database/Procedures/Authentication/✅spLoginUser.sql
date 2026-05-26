22e78426e3efc2922758724e4dba6d6aea972b8b
SUJIT KAR <sujitkar2005@gmail.com>
2026-05-26 09:41:59 +0530

    STORE PROC UPDATE

--- Database/Procedures/Authentication/✅spLoginUser.sql
+++ Database/Procedures/Authentication/✅spLoginUser.sql
@@ -1,46 +1,47 @@
-CREATE PROCEDURE spLoginUser  
+CREATE PROCEDURE spLoginUser    
+ 
+    @Email VARCHAR(100),    
+    @Password VARCHAR(MAX)    
 
-    @Email VARCHAR(100),  
-    @Password VARCHAR(MAX)  
-
-AS  
-BEGIN  
+AS    
+BEGIN    
+      
+    DECLARE @UserID INT;  
+  
+    IF EXISTS    
+    (    
+        SELECT 1    
+        FROM tblUserContact C    
+        INNER JOIN tblUserAuthentication A    
+            ON C.UserID = A.UserID    
+        WHERE     
+            C.Email = @Email    
+            AND A.Password = @Password    
+    )    
+    BEGIN    
     
-    DECLARE @UserID INT;
-
-    IF EXISTS  
-    (  
-        SELECT 1  
+        SELECT @UserID = C.UserID  
         FROM tblUserContact C  
-        INNER JOIN tblUserAuthentication A  
+        INNER JOIN tblUserAuthentication A    
             ON C.UserID = A.UserID  
-        WHERE   
-            C.Email = @Email  
-            AND A.Password = @Password  
-    )  
-    BEGIN  
-
-        SELECT @UserID = C.UserID
-        FROM tblUserContact C
-        INNER JOIN tblUserAuthentication A  
-            ON C.UserID = A.UserID
-        WHERE   
-            C.Email = @Email  
-            AND A.Password = @Password;
-
-        UPDATE tblUserAuthentication
-        SET Active = 1
-        WHERE UserID = @UserID;
-
-        PRINT 'Login Successful';
-
-        SELECT @UserID AS UserID;
-    END  
-    
-    ELSE  
-    BEGIN  
-        PRINT 'Invalid Email Or Password';  
-    END  
-
+        WHERE     
+            C.Email = @Email    
+            AND A.Password = @Password;  
+  
+      
+        UPDATE tblUserAuthentication  
+        SET Active = 1  
+        WHERE UserID = @UserID;  
+  
+        SELECT 
+            'Login Successful' AS Message,
+            @UserID AS UserID;  
+  
+    END    
+      
+    ELSE    
+    BEGIN    
+        SELECT 'Invalid Email Or Password' AS Message;    
+    END    
+  
 END;
-
