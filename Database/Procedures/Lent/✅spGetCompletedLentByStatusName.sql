b573f9e822fcb06fc8937b5be3b872836c4454ae
Sk Akhmal Hossain <skakhmalhossain@gmail.com>
2026-05-27 11:55:27 +0530

    SpInsertLent

--- Database/Procedures/Lent/✅spGetCompletedLentByStatusName.sql
+++ Database/Procedures/Lent/✅spGetCompletedLentByStatusName.sql
@@ -2,7 +2,15 @@ CREATE PROC spGetCompletedLentByStatusName
 @UserID INT
 AS
 BEGIN
-	SELECT Prsn.PersonName, L.Amount, L.ReturnedAmount, L.RemainingAmount, Pay.PaymentName, S.StatusName, L.LentAt, L.DeadlineAt, L.Description
+	SELECT Prsn.PersonName,
+		   L.Amount,
+		   L.ReturnedAmount,
+		   L.RemainingAmount,
+		   Pay.PaymentName,
+		   S.StatusName,
+		   L.LentAt,
+		   L.DeadlineAt,
+		   L.Description
 	FROM tblLent L
 	LEFT JOIN tblLentBorrowStatus S ON L.StatusID = S.StatusID
 	LEFT JOIN tblLentPersons Prsn ON L.PersonID = Prsn.PersonID
