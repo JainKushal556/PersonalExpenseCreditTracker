0c7ce720f6059cd1e5460ce8039e7d0c9cf639d6
Arpita Jana <titlijana1234@gmail.com>
2026-05-27 23:34:11 +0530

    created spGetTodayCredit

---
+++ Database/Procedures/Credit/✅spGetTodayCredit.sql
@@ -0,0 +1,59 @@
+CREATE PROCEDURE spGetTodayCredit
+(
+    @UserID INT
+)
+AS
+BEGIN
+    SET NOCOUNT ON
+
+    IF NOT EXISTS
+    (
+        SELECT 1
+        FROM tblUserAuthentication UserAuthentication
+        WHERE UserAuthentication.UserID = @UserID
+        AND UserAuthentication.Active = 1
+    )
+    BEGIN
+        SELECT 'Invalid Or Inactive User' AS Message
+        RETURN
+    END
+
+    IF NOT EXISTS
+    (
+        SELECT 1
+        FROM tblCredit
+        WHERE UserID = @UserID
+        AND CAST(CreditAt AS DATE) = CAST(GETDATE() AS DATE)
+    )
+    BEGIN
+        SELECT 'No Record Found' AS Message
+        RETURN
+    END
+
+    SELECT
+        Credit.CreditID,
+        CreditCategory.CategoryName,
+        CreditSubCategory.SubCategoryName,
+        Credit.Amount,
+        LTRIM(RTRIM(Credit.Description)) AS Description,
+        PaymentType.PaymentName,
+        Credit.CreditAt
+
+         FROM tblCredit Credit
+
+          LEFT JOIN tblCreditCategory CreditCategory
+               ON Credit.CategoryID = CreditCategory.CategoryID
+
+          LEFT JOIN tblCreditSubCategory CreditSubCategory
+                ON Credit.SubCategoryID = CreditSubCategory.SubCategoryID
+
+             LEFT JOIN tblPaymentType PaymentType
+               ON Credit.PaymentID = PaymentType.PaymentID
+
+             WHERE Credit.UserID = @UserID
+                 AND CAST(Credit.CreditAt AS DATE) = CAST(GETDATE() AS DATE)
+
+              ORDER BY Credit.CreditAt DESC
+
+END
+GO
