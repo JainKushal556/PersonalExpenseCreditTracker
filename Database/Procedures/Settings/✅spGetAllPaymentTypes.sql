7ed5aefd90661eb88a4e78d4bdd40d9de200ad7c
Arpita Jana <titlijana1234@gmail.com>
2026-05-28 14:14:32 +0530

    update spGetAllPaymentTypes

---
+++ Database/Procedures/Settings/✅spGetAllPaymentTypes.sql
@@ -0,0 +1,24 @@
+CREATE PROCEDURE spGetAllPaymentTypes
+AS
+BEGIN
+
+    SET NOCOUNT ON;
+
+    IF NOT EXISTS
+    (
+        SELECT 1
+        FROM tblPaymentType
+    )
+    BEGIN
+        SELECT 'No Payment Type Found' AS Message
+        RETURN
+    END
+
+    SELECT
+        PaymentID,
+        PaymentName
+    FROM tblPaymentType
+    ORDER BY PaymentName ASC;
+
+END
+GO
