09aa72bdf7f1d4325beb7ea6baca0ffec1537163
Kushal Jain <jainkushal556@gmail.com>
2026-08-06 23:00:14 +0530

    Merged Note logic updates from feature/expense-settings while preserving search feature

--- WinFormsApp/PersonalExpenseCreditTracker/DALayer/Note/NoteDAL.cs
+++ WinFormsApp/PersonalExpenseCreditTracker/DALayer/Note/NoteDAL.cs
@@ -2,10 +2,61 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using DALayer.Common;
 
 namespace DALayer.Note
 {
-    class NoteDAL
+    public class NoteDAL
     {
+        public int userId { get; set; }
+        public int noteId { get; set; }
+        public string noteTitle { get; set; }
+        public string description { get; set; }
+        public int priorityId { get; set; }
+        public int colorId { get; set; }
+        //public string colorHexCode { get; set; }
+
+        private Boolean ReturnBoolean(int value)
+        {
+            if (value > 0) return true;
+            else return false;
+        }
+        public Boolean SaveNoteToDb()
+        {
+            string connectionstring = Common.SqlHelper.connectionString;
+            SqlConnection sqlConnection = null;
+            int rowsEffected = 0;
+            try
+            {
+                sqlConnection = new SqlConnection(connectionstring);
+                using (SqlCommand sqlCommand = new SqlCommand("spInsertNote", sqlConnection))
+                {
+                    sqlCommand.CommandType = CommandType.StoredProcedure;
+                    sqlCommand.Parameters.AddWithValue("@UserID", userId);
+                    sqlCommand.Parameters.AddWithValue("@PriorityID", priorityId);
+                    sqlCommand.Parameters.AddWithValue("@NoteColorID", colorId);
+                    sqlCommand.Parameters.AddWithValue("@NoteTitle", noteTitle);
+                    sqlCommand.Parameters.AddWithValue("@Description", description);
+
+                    sqlConnection.Open();
+                    rowsEffected = sqlCommand.ExecuteNonQuery();
+                    return ReturnBoolean(rowsEffected);
+                }
+            }
+            catch (Exception ex)
+            {
+                return ReturnBoolean(rowsEffected);
+            }
+            finally
+            {
+                if (sqlConnection != null)
+                {
+                    sqlConnection.Close();
+                }
+            }
+        }
     }
 }
