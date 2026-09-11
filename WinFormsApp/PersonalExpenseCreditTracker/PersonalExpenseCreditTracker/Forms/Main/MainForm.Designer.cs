d1af3406dc7adcf3fe992b9df190399aaae4c131
SUJIT KAR <sujitkar2005@gmail.com>
2026-09-11 09:39:22 +0530

    Update MainForm

--- WinFormsApp/PersonalExpenseCreditTracker/PersonalExpenseCreditTracker/Forms/Main/MainForm.Designer.cs
+++ WinFormsApp/PersonalExpenseCreditTracker/PersonalExpenseCreditTracker/Forms/Main/MainForm.Designer.cs
@@ -2088,7 +2088,6 @@ namespace PersonalExpenseCreditTracker
             this.Controls.Add(this.pnlMainSideBarSection);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(5);
-            this.MaximizeBox = false;
             this.Name = "MainForm";
             this.Text = "Personal Expense Manager";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
